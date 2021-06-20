29edb4400e9223a0c06c1c3e62c8e530ff9a86f2
Batnaidan Tsend <batnaidan@gmail.com>
2021-06-21 00:38:54 +0800

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import Button from '@material-ui/core/Button';
 
@@ -6,19 +6,33 @@ import GoogleSignIn from './GoogleSignIn';
 import EmailSignIn from './EmailSignIn';
 
 export default function Login() {
+  const [didClickContinue, changeClickContinue] = useState(false);
+
+  const handleContinueClick = () => {
+    changeClickContinue(true);
+  };
+
   return (
     <div className="login-wrapper">
-      <GoogleSignIn></GoogleSignIn>
-      <hr className="h-line"></hr>
-      <EmailSignIn></EmailSignIn>
-      <Button
-        variant="contained"
-        color="primary"
-        className="login-button"
-        style={{ fontSize: '1em' }}
-      >
-        CONTINUE
-      </Button>
+      {didClickContinue ? (
+        <div>hello</div>
+      ) : (
+        <div>
+          {' '}
+          <GoogleSignIn></GoogleSignIn>
+          <hr className="h-line"></hr>
+          <EmailSignIn></EmailSignIn>
+          <Button
+            variant="contained"
+            color="primary"
+            className="login-button"
+            style={{ fontSize: '1em', backgroundColor: 'hsl(33, 94%, 51%)' }}
+            onClick={handleContinueClick}
+          >
+            CONTINUE
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
